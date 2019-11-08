4ce4b6823767d3d81792d07468fd2feaf49a3bd7
1803031 <37138507+work-nchc@users.noreply.github.com>
2019-11-08 13:14:12 +0800

    Add files via upload

--- concat.py
+++ concat.py
@@ -1,21 +1,21 @@
-from numpy import load, save, zeros
+from numpy import load, save, zeros, nan_to_num
 from sklearn.neighbors import KNeighborsRegressor
 from open3d import read_point_cloud
 
 vox1 = read_point_cloud('vox1.ply')
 neigh = KNeighborsRegressor(1, n_jobs=-1)
 
-pca = load('pca_1.npy')
+pca = nan_to_num(load('pca_1.npy'))
 pca_concat = zeros((len(pca), 9))
 pca_concat[:,:3] = pca
 
 vox = read_point_cloud('vox2.ply')
-pca = load('pca_2.npy')
+pca = nan_to_num(load('pca_2.npy'))
 neigh.fit(vox.points, pca)
 pca_concat[:,3:6] = neigh.predict(vox1.points)
 
 vox = read_point_cloud('vox4.ply')
-pca = load('pca_4.npy')
+pca = nan_to_num(load('pca_4.npy'))
 neigh.fit(vox.points, pca)
 pca_concat[:,6:] = neigh.predict(vox1.points)
 
